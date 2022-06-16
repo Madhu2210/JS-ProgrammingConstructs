4f27295435266e4ddc7fd71ff657e8c34bf3c59c
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-06-16 18:43:00 +0530

    Added: Adding FlipCoin Problem with WhileLoop.

---
+++ FlipCoinWhileLoop.js
@@ -0,0 +1,13 @@
+let head = 0;
+let tail = 0;
+while ((head < 11) && (tail < 11)) {
+    let result = Math.floor(Math.random() * 10) % 2;
+    console.log(result);
+    if (result == 0) {
+        head = head + 1;
+    } else {
+        tail = tail + 1;
+    }
+}
+console.log("Head Occured " + head + " Times ");
+console.log("Tail Occured " + tail + " Times ");
